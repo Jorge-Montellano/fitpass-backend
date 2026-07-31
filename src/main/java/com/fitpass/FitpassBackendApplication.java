af67a616b76f3cc62e59a4487f2ff0276fa7755a
Jorge Montellano <jorgemontellanoweb@gmail.com>
2026-07-30 21:58:02 -0400

    Initial commit: Spring Boot 4 FitPass project

---
+++ src/main/java/com/fitpass/FitpassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitpassBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitpass/FitpassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitpassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
