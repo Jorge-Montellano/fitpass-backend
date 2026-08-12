1d2ab2a7d0f9c18f30abb0109a08a31f2373d910
Jorge Montellano <jorgemontellanoweb@gmail.com>
2026-08-11 23:57:59 -0400

    feat: add API Gateway routing to monolith and booking service

---
+++ fitpass-api-gateway/src/main/java/fitpass_api_gateway/FitpassApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package fitpass_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpassApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitpassApiGatewayApplication.class, args);
+	}
+
+}
